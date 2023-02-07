211575f74b87bd3dff61e8d059a89a865f9d3ffd
Andrew Caruso <10090913+Yzma@users.noreply.github.com>
2023-02-07 11:23:29 -0500

    Update students and teachers classes to sanitize user inputs

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -2,33 +2,29 @@ const { Pool } = require('pg');
 
 const args = process.argv.slice(2);
 
-console.log('1')
 const pool = new Pool({
   user: 'labber',
   password: 'labber',
   host: 'localhost',
   database: 'bootcampx'
 });
+
+const cohortName = args[0];
+const limit = args[1] || 5;
+const values = [`%${cohortName}%`, limit];
+console.log('values', values)
+
 const query = `
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${args[0]}%'
-LIMIT ${parseInt(args[1])};
+WHERE cohorts.name LIKE $1
+LIMIT $2;
 `
 
-console.log(query)
-
-pool.query(query)
+pool.query(query, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
   })
 }).catch((err) => console.log(err));
-
-// CREATE TABLE cohorts (
-//   id SERIAL PRIMARY KEY NOT NULL,
-//   name VARCHAR(255) NOT NULL,
-//   start_date DATE,
-//   end_date DATE
-// );
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -9,19 +9,22 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-console.log('args ', args)
+const cohortName = args[0];
+const values = [`${cohortName}`];
+console.log('values', values)
+
 const query = `
 SELECT DISTINCT teachers.name, cohorts.name AS cohort_name
 FROM assistance_requests
 JOIN teachers ON teachers.id = assistance_requests.teacher_id
 JOIN students ON students.id = assistance_requests.student_id
 JOIN cohorts ON cohorts.id = students.cohort_id
-WHERE cohorts.name = '${args[0]}'
+WHERE cohorts.name = $1
 GROUP BY cohorts.name, teachers.name
 ORDER BY teachers.name;
 `
 
-pool.query(query)
+pool.query(query, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.cohort_name}: ${user.name}`);
